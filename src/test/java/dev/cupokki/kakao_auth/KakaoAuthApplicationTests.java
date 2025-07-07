073ba57ff09182491d9b204f71f43a2fadd9c4bd
cupokki <nd1346@gmail.com>
2025-07-07 23:31:00 +0900

    Initial commit

---
+++ src/test/java/dev/cupokki/kakao_auth/KakaoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cupokki.kakao_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
