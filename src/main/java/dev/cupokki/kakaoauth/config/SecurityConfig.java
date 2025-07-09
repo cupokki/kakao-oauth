38e4a151425f7a88c89088efb56c031dbbf40dcd
cupokki <nd1346@gmail.com>
2025-07-09 23:28:30 +0900

    chore: 시큐리티 설정

--- src/main/java/dev/cupokki/kakaoauth/config/SecurityConfig.java
+++ src/main/java/dev/cupokki/kakaoauth/config/SecurityConfig.java
@@ -3,22 +3,31 @@ package dev.cupokki.kakaoauth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .cors(cors->cors.disable())
                 .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.sameOrigin()))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf->csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 //                .addFilterBefore()
-                .csrf(csrf->csrf.disable())
                 .build();
     }
 }
--- src/main/java/dev/cupokki/kakaoauth/controller/AuthController.java
+++ src/main/java/dev/cupokki/kakaoauth/controller/AuthController.java
@@ -3,6 +3,7 @@ package dev.cupokki.kakaoauth.controller;
 import dev.cupokki.kakaoauth.service.Authservice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class AuthController {
 
     private final Authservice authservice;
 
-    @PostMapping("/auth/login")
+    @GetMapping("/auth/login")
     public ResponseEntity<?> login() {
         return ResponseEntity.ok(null);
     }
