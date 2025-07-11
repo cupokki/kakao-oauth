4c7f662878e90e0d834d0b26be4dc5d8b11db7c5
cupokki <nd1346@gmail.com>
2025-07-12 00:26:14 +0900

    chore: 필터 설정

--- src/main/java/dev/cupokki/kakaoauth/config/SecurityConfig.java
+++ src/main/java/dev/cupokki/kakaoauth/config/SecurityConfig.java
@@ -1,20 +1,27 @@
 package dev.cupokki.kakaoauth.config;
 
+import dev.cupokki.kakaoauth.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         return http
                 .cors(cors->cors.disable())
                 .formLogin(form -> form.disable())
@@ -27,7 +34,7 @@ public class SecurityConfig {
                 )
                 .csrf(csrf->csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .addFilterBefore()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
